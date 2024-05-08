9c939eff920647daee210558050ecd87f8045ae8
vargasjuls <161017083+vargasjuls@users.noreply.github.com>
2024-05-08 11:09:46 +0800

    Add files via upload

---
+++ JULS_CRUD/ACT1A_CRUD/Admin.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace ACT1A_CRUD
+{
+    public partial class Admin : Form
+    {
+        private MySqlConnection connection;
+        public Admin()
+        {
+            InitializeComponent();
+            connection = new MySqlConnection("server=localhost;database=eiramdb;username=root;password=;");
+        }
+
+        private void Admin_Load(object sender, EventArgs e)
+        {
+            loaddata();
+        }
+
+        private void loaddata()
+        {
+            try
+            {
+                connection.Open();
+                string showallrecordquery = "SELECT id, username, name, role FROM yuser ORDER BY ID ASC";
+                MySqlCommand command = new MySqlCommand(showallrecordquery, connection);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+            finally
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+          
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                txtid.Text = row.Cells["ID"].Value.ToString();
+                txtname.Text = row.Cells["NAME"].Value.ToString();
+                txtusername.Text = row.Cells["USERNAME"].Value.ToString();
+                cbrole.Text = row.Cells["ROLE"].Value.ToString();
+            }
+        }
+    }
+}
